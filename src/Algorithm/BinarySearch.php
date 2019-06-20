49a8cc96e6878bd62e2856ddfbe03f68a217b8c9
wangan <wangan@qutoutiao.net>
2019-06-20 18:30:13 +0800

    二分查找

--- src/Algorithm/BinarySearch.php
+++ src/Algorithm/BinarySearch.php
@@ -7,12 +7,72 @@
  */
 
 namespace algorithm;
-
-
+/**
+ * 实现对一个数组的二分查找
+ * Class BinarySearch
+ * @package algorithm
+ */
 class BinarySearch
 {
-    public function test()
+    /**
+     * 使用循环实现, 返回index
+     * @param array $findDate
+     * @param int $searchVal
+     * @return int
+     */
+    public static function binarySearch(array $findDate, int $searchVal)
     {
-        echo "test";
+        $count = count($findDate);
+        if($count<1){
+            return -1;
+        }
+        $low = 0;
+        $high = $count - 1;
+        while ($high >= $low){
+            $mid = (int)(($low+$high)/2);
+            if ($findDate[$mid] == $searchVal){
+                return $mid;
+            }
+            if($findDate[$mid] > $searchVal){
+                $high = $mid-1;
+            }
+            if($findDate[$mid] < $searchVal){
+                $low = $mid+1;
+            }
+        }
+        return -1;
     }
+
+    /**
+     * 递归实现
+     * 终止条件：找到了值，或者low>high
+     * @param array $searchDate
+     * @param int $searchVal
+     * @param int $low
+     * @param int $high
+     * @return int
+     */
+    public static function binarySearchInternally(array $searchDate, int $searchVal, int $low, int $high)
+    {
+        if($low>$high){
+            return -1;
+        }
+
+        $mid = (int)(($low + $high)/2);
+
+        if($searchDate[$mid] == $searchVal){
+            return $mid;
+        }
+
+        if($searchDate[$mid] > $searchVal){
+            $high = $mid-1;
+            return binarySearchInternally($searchDate, $searchVal, $low, $high);
+        }
+
+        if($searchDate[$mid] < $searchVal){
+            $low = $mid+1;
+            return binarySearchInternally($searchDate, $searchVal, $low, $high);
+        }
+    }
+
 }
