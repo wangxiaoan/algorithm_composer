9d17fd87452449cd98502f5446a1477ebd443966
wangan <wangan@qutoutiao.net>
2019-06-20 11:09:34 +0800

    init

---
+++ src/Algorithm/BinarySearch.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2019/6/20
+ * Time: 10:54
+ */
+
+namespace algorithm;
+
+
+class BinarySearch
+{
+    public function test()
+    {
+        echo "test";
+    }
+}
